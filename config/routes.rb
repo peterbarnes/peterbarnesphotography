8dc20d7c9016e22ffad1f7e2ab7670e452c5d312
peterbarnes <barnespeter213@gmail.com>
2015-01-04 11:11:29 -0600

    Add pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'static#index'
+  match '/contact',   to: 'static#contact',   via: 'get'
+  match '/events',    to: 'static#events',    via: 'get'
+  match '/landscape', to: 'static#landscape', via: 'get'
+  match '/video',     to: 'static#video',     via: 'get'
 end
